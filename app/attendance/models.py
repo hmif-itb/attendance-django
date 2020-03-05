8df7297109bc9dabdf379963173f6a5bd51d4801
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-03-05 13:02:25 +0700

    Add keyboard shortcut model

--- app/attendance/models.py
+++ app/attendance/models.py
@@ -3,6 +3,22 @@ from event.models import Event
 
 # Create your models here.
 
+class AttendeeMajor(models.Model):
+    name = models.CharField(max_length=100)
+    nim_prefix = models.CharField(max_length=3)
+    keyboard_shortcut = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f'{self.nim_prefix} - {self.name}'
+
+class AttendeeYear(models.Model):
+    name = models.CharField(max_length=100)
+    nim_prefix = models.CharField(max_length=3)
+    keyboard_shortcut = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f'{self.nim_prefix} - {self.name}'
+
 class Attendee(models.Model):
     nim = models.CharField(max_length=8, primary_key=True)
     name = models.CharField(max_length=200)
